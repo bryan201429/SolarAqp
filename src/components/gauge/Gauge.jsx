5ee891ca05597ad998e8e186a44c3e45c833ee78
Nayrb201429 <bryan201429@gmail.com>
2024-02-23 23:47:17 -0500

    chartjsinstall

--- src/components/gauge/Gauge.jsx
+++ src/components/gauge/Gauge.jsx
@@ -1,29 +1,72 @@
-import React, { useEffect, useState } from 'react';
-import './gauge.css';
+import React, { useEffect } from 'react';
+import Chart from 'chart.js/auto';
 
-export default function Gauge() {
-  const [numeroUV, setNumeroUV] = useState(0);
-  const [degrees, setDegrees] = useState(0);
-
+const Gauge = () => {
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      setNumeroUV(5.33); // Aquí estableces el número UV como 15 cada 2 segundos
-    }, 1000);
+    // setup 
+    const data = {
+      labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+      datasets: [{
+        label: 'Weekly Sales',
+        data: [18, 12, 6, 9, 12, 3, 9],
+        backgroundColor: [
+          'rgba(255, 26, 104, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(0, 0, 0, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 26, 104, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(0, 0, 0, 1)'
+        ],
+        borderWidth: 1
+      }]
+    };
 
-    return () => clearInterval(intervalId);
-  }, []); // La dependencia es un arreglo vacío, lo que significa que se ejecuta solo una vez al montar el componente
+    // config 
+    const config = {
+      type: 'doughnut',
+      data,
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    };
 
-  useEffect(() => {
-    const newDegrees = Math.round((numeroUV / 15) * 180);
-    setDegrees(newDegrees);
-  }, [numeroUV]); // Esta dependencia asegura que se recalcule 'degrees' cada vez que 'numeroUV' cambie
+    // render init block
+    const myChart = new Chart(
+      document.getElementById('myChart'),
+      config
+    );
+
+    // Clean up
+    return () => {
+      myChart.destroy();
+    };
+  }, []);
 
   return (
-    <div className="gaugeContainer">
-      <h1 className="loader__title">{numeroUV}</h1>
-
-      <div className="loader" style={{ transform: `rotate(${degrees}deg)` }}></div>
-
-    </div>
+    <>
+      <div className="chartMenu">
+      </div>
+      <div className="chartCard">
+        <div className="chartBox">
+          <canvas id="myChart"></canvas>
+        </div>
+      </div>
+    </>
   );
-}
+};
+
+export default Gauge;
