41249b7c45cf989f6f65497cb948b76b09d7c800
Nayrb201429 <bryan201429@gmail.com>
2024-07-19 23:28:33 -0500

    bar info

--- src/components/chartInfo/ChartInfoBox.jsx
+++ src/components/chartInfo/ChartInfoBox.jsx
@@ -6,11 +6,42 @@ import { useState } from 'react';
 export default function ChartInfoBox(){
     let ChartInfoBodyData1 = (
         <div className='info1'>
-            <h1>Radiación mas alta:</h1>
-            <h1>Hora de pico de radiación:</h1>
-            <h1>Temperatura mas alta: </h1>
-            <h1>Temperaturas mas baja:</h1>
+            <div className='containerInfo'>
+                <h1>Indice UV mas alta:</h1><h1>1</h1>
+            </div>
+
+                <hr className='divider'></hr>  
+
+            <div className='containerInfo'>
+                <h1>Hora de pico de radiación:</h1> <h1>1</h1>
+            </div>
+
+                <hr className='divider'></hr>  
+
+            <div className='containerInfo'> 
+                <h1>Temperatura mas alta: </h1> <h1>1</h1>
+            </div>
+                <hr className='divider'></hr>  
+            <div className='containerInfo'> 
+                <h1>Temperaturas mas baja:</h1> <h1>1</h1>
+            </div>
+            <hr className='divider'></hr>  
+            <div className='containerInfo'> 
+            <h1>Humedad mas alta: </h1> <h1>1</h1>
+            </div>
+            <hr className='divider'></hr>  
+
+            <div className='containerInfo'> 
+            <h1>Humedad mas baja:</h1> <h1>1</h1>
+            </div>
+
+             
+                
+   
+                
         </div>
+
+
     )
     let ChartInfoBodyData2 = (
         <div>
@@ -37,10 +68,10 @@ export default function ChartInfoBox(){
         <div id='ButtonContainer'> 
                         <button id='button1' className='infoButton1' onClick={buttonChange1}> Estadísticas de Hoy</button>
                         <button id='button2' className='infoButton2 infoButtonInactive2' onClick={buttonChange2}>Consultar por fecha</button>
-                    </div>
+        </div>
                     <div id='ChartInfoBody'>
                         {chartInfoBodyData}
-        </div>
+                    </div>
         </>
     )
 
